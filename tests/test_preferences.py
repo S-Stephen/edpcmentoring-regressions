44942d6789a12de5147403d1275c3f8d590c45b4
S-Stephen <stephen.shorrock@gmail.com>
2016-07-21 11:46:40 +0100

    exported test using reconfigured python export header

--- tests/test_preferences.py
+++ tests/test_preferences.py
@@ -6,42 +6,42 @@ from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
-import os
 
-class ChangePreferences(unittest.TestCase):
+class TestPreferences(unittest.TestCase):
     def setUp(self):
         username = os.environ["SAUCE_USERNAME"]
-        access_key = os.environ["SAUCE_ACCESS_KEY"] 
-        #desired_cap["tunnel-identifier"] = os.environ["TRAVIS_JOB_NUMBER"]
+        access_key = os.environ["SAUCE_ACCESS_KEY"]
         desired_cap = {
             'platform': "Mac OS X 10.9",
             'browserName': "chrome",
             'version': "31",
         }
-        hub_url = "%s:%s@localhost:4445" % (username, access_key) 
+        hub_url = "%s:%s@localhost:4445" % (username, access_key)
         self.driver = webdriver.Remote(desired_capabilities=desired_cap, command_executor="http://%s/wd/hub" % hub_url)
-        #self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(30)
-        self.base_url = "http://cupcmentoring.herokuapp.com/"
+        self.base_url = "https://demo.raven.cam.ac.uk/"
         self.verificationErrors = []
         self.accept_next_alert = True
     
-    def test_change_preferences(self):
+    def test_preferences(self):
         driver = self.driver
-        driver.get("http://cupcmentoring.herokuapp.com/")
-        #driver.get(self.base_url + "/auth/authenticate.html?ver=3&url=http%3A%2F%2Fcupcmentoring.herokuapp.com%2Fraven_return%2F&desc=&iact=&msg=&params=next%3D%252F&fail=")
-        driver.find_element_by_id("userid").clear()
-        driver.find_element_by_id("userid").send_keys("test")
+        driver.get(self.base_url + "/auth/authenticate.html?ver=3&url=http%3A%2F%2Fcupcmentoring.herokuapp.com%2Fraven_return%2F&desc=&iact=&msg=&params=next%3D%252F&fail=")
         driver.find_element_by_id("userid").clear()
         driver.find_element_by_id("userid").send_keys("test0001")
         driver.find_element_by_id("pwd").clear()
         driver.find_element_by_id("pwd").send_keys("test")
         driver.find_element_by_name("submit").click()
         driver.find_element_by_link_text("Change these preferences").click()
+        # ERROR: Caught exception [ERROR: Unsupported command [selectWindow | null | ]]
+        try: self.assertEqual("off", driver.find_element_by_id("id_is_seeking_mentor").get_attribute("value"))
+        except AssertionError as e: self.verificationErrors.append(str(e))
         driver.find_element_by_id("id_is_seeking_mentor").click()
+        try: self.assertEqual("on", driver.find_element_by_id("id_is_seeking_mentor").get_attribute("value"))
+        except AssertionError as e: self.verificationErrors.append(str(e))
         driver.find_element_by_id("submit-id-submit").click()
         driver.find_element_by_link_text("Change these preferences").click()
         driver.find_element_by_id("id_is_seeking_mentor").click()
+        driver.get("https://demo.raven.cam.ac.uk/auth/logout.html")
     
     def is_element_present(self, how, what):
         try: self.driver.find_element(by=how, value=what)
@@ -70,4 +70,3 @@ class ChangePreferences(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
-
